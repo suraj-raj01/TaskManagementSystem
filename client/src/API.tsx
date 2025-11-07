929f0c21e2ba58eced8565be03518356e7cbd4dd
SURAJ_KUMAR <surajk38656@gmail.com>
2025-11-07 11:55:49 +0530

    Backend deployed successfully

--- client/src/API.tsx
+++ client/src/API.tsx
@@ -1,5 +1,5 @@
 // const api = 'http://localhost:8000/api';
 // const api = import.meta.env.VITE_API_URL;
-const api = 'https://tsm-3xh9.onrender.com/api';
+const api = 'https://tsm-1-oqec.onrender.com/api';
 // const api = 'http://localhost:3000/api';
 export default api;
