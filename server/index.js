3c030301b11b628998df1a3d28ec9a87fb90c967
SURAJ <surajk38656@gmail.com>
2026-01-17 14:44:53 +0530

    SERVER UPDATED

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ const http = require("http");
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173", // React dev server
+    origin: "https://tsm-w2rl.vercel.app", // React dev server
     methods: ["GET", "POST"],
   },
 });
